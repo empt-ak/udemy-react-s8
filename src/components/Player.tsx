e8efc0604d1396b236cd0d144f8a8f251be002d3
Dominik Szalai <emptulik@gmail.com>
2024-12-17 20:56:47 +0100

    p131

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,10 +1,22 @@
+import {useState} from 'react'
+
 const Player = () => {
+  const [name, setName] = useState<string>('')
+  const [submitted, setSubmitted] = useState<boolean>(false)
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? name : 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" value={name} onChange={(e) => {
+          if(name && submitted) {
+            setSubmitted(false)
+          }
+          setName(e.currentTarget.value)
+        }}/>
+        <button onClick={() => {
+          setSubmitted(true)
+        }}>Set Name</button>
       </p>
     </section>
   )
